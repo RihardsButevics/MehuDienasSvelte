2a89699049cf1709183e60b723e2bcd447a5eb2d
Something Playz <RihardsButevics@gmail.com>
2023-06-07 00:27:38 +0300

    v1.1

---
+++ src/lib/Galerija.svelte
@@ -0,0 +1,53 @@
+<script>
+  import Youtube from "svelte-youtube-embed";
+
+</script>
+
+
+<div class="divider"></div>
+<h1 class="text-2xl justify-around m-auto text-center ">PIE MUMS PAVIESOJĀS DAŽI LATVIJAS YOUTUBERI</h1>
+<div class="max-w-6xl  grid grid-cols-1 lg:grid-cols-2 m-auto  ">
+
+  <div class="p-4 bg-base-300 rounded-box m-3">
+    <Youtube id="26PMPkn0WxY" />
+  
+  </div>
+  
+  <div class="p-4 bg-base-300 rounded-box m-3">
+    <Youtube id="qf7kkeGtqao" />
+  
+  </div>
+  
+  
+  <div class="p-4 bg-base-300 rounded-box m-3">
+    <Youtube id="F7ZszEbyoPs" />
+  </div>
+ 
+<div class="p-4 bg-base-300 rounded-box m-3">
+  <Youtube id="fgtGg96XWmc" />
+
+</div>
+</div>
+
+<div class="divider"></div>
+<h1 class="text-2xl justify-around m-auto text-center ">Mehu Dienas 2023 Galerijas</h1>
+<div class="max-w-6xl  grid grid-cols-1 lg:grid-cols-2 m-auto p-5  ">
+  <img class="p-5" src="/img/Galerija/1.jpg" alt=""><img  class="p-5" src="/img/Galerija/2.jpg" alt=""><img  class="p-5" src="/img/Galerija/3.jpg" alt=""><img  class="p-5" src="/img/Galerija/4.jpg" alt=""><img class="p-5"  src="/img/Galerija/5.jpg" alt=""><img class="p-5"  src="/img/Galerija/6.jpg" alt="">
+
+
+</div>
+
+<div class=" grid grid-cols-1 lg:grid-cols-3 m-auto max-w-6xl ">
+  <div class="btn bg-red-700 text-white lg:w-32 w-64 m-auto  "> 
+    2023. gada galerija 
+  </div>
+  <div class="btn bg-red-700 text-white lg:w-32 w-64 m-auto "> 
+    2019. gada galerija 
+  </div>
+  <div class="btn bg-red-700 text-white lg:w-32 w-64 m-auto "> 
+    2018. gada galerija
+  </div>
+</div>
+
+
+
--- src/routes/Galerija/+page.svelte
+++ src/routes/Galerija/+page.svelte
@@ -1,13 +1,18 @@
 <script>
 	import NavBar from '$lib/NavBar.svelte';
 	import Footer from '$lib/Footer.svelte';
-	import MehuBurti from '$lib/MehuBurti.svelte';
+	import Galerija from '$lib/Galerija.svelte';
 </script>
 
 <NavBar />
 
-<h1 class="text-5xl justify-around m-auto text-center p-10">GALERIJA</h1>
+<h1 class="text-5xl justify-around m-auto text-center ">GALERIJA</h1>
 
-<div />
+
+
+<Galerija />
+
+
+
 
 <Footer />
